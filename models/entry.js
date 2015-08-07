42e6b5541057ff17611809bb8d904782a3bf5655
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2015-08-07 16:30:52 -0700

    updated models/entry.js to include schema

--- models/entry.js
+++ models/entry.js
@@ -1,1 +1,9 @@
-// Entry model
+var mongoose = require('mongoose');
+
+var EntrySchema = new mongoose.Schema({
+  name: String,
+  img_url: String,
+  date_posted: Date
+});
+
+mongoose.model('Entry', EntrySchema);
